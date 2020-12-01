15bc27cbba078c3539cf996f9e4dd505853b2062
Zack <sligerzack@gmail.com>
2020-12-01 16:02:04 -0500

    Delete Sachin's Day 1
    Thanks for the template

--- src/day-1/index.js
+++ src/day-1/index.js
@@ -1,43 +1,9 @@
 import filereader from '../utils/filereader.js';
 
-const inputArray = filereader('input.txt').map(Number);
+const inputArray = filereader('input.txt');
 
 const solve = () => {
-    // Tim sort o(n * log(n))
-    inputArray.sort((a, b) => a - b);
 
-    // part 1 - O(n * log(n)) time including the sort
-    let sum, startPointer = 0;
-    let endPointer = inputArray.length - 1;
-    while (startPointer < endPointer) {
-        sum = inputArray[startPointer] + inputArray[endPointer];
-        if (sum === 2020) {
-            console.log('part 1 sol', inputArray[startPointer] * inputArray[endPointer]);
-            break;
-        } else if (sum > 2020) {
-            endPointer--;
-        } else {
-            startPointer++;
-        }
-    }
-
-    // part 2 - O(n^2) time including the sort
-    sum = 0, startPointer = 0;
-    endPointer = inputArray.length - 1;
-    while (startPointer < endPointer && sum !== 2020) {
-        for (let thirdPointer = startPointer + 1; thirdPointer < endPointer; thirdPointer++) {
-            sum = inputArray[startPointer] + inputArray[endPointer] + inputArray[thirdPointer];
-            if (sum === 2020) {
-                console.log('part 2 sol', inputArray[startPointer] * inputArray[endPointer] * inputArray[thirdPointer]);
-                break;
-            }
-        }
-        if (sum > 2020) {
-            endPointer--;
-        } else {
-            startPointer++;
-        }
-    }
 }
 
 solve();
